bed844ca0ac4a1185311864266d2868637c3cad6
feliciaszhang <feliciaszhang@gmail.com>
2021-02-11 21:37:57 -0500

    cleanup

--- lib/useEvents.ts
+++
@@ -1,9 +0,0 @@
-import useSWR from "swr";
-
-export const useEvents = (user) => {
-  const { data: events } = useSWR(user?.isLoggedIn && `/api/events`);
-  const loadingEvents = events === undefined;
-  return { events, loadingEvents };
-}
-
-export default useEvents;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,8 @@ import Program, { ProgramProps } from "../components/Program";
 import prisma from "../lib/prisma";
 import { Heading, Box, Text } from "@chakra-ui/react";
 import Layout from "../components/Layout";
-import useEvents from "../lib/useEvents";
-import useUser from "../lib/useUser";
 import useProgram from "../lib/useProgram";
+import useUser from "../lib/useUser";
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const programs = await prisma.program.findMany({
@@ -24,9 +23,7 @@ type Props = {
 };
 
 const Index: React.FC<Props> = ({ programs }) => {
-  const { user } = useUser({ redirectTo: "/signin" });
   const { program } = useProgram();
-  const { events, loadingEvents } = useEvents(user);
   return (
     <Layout>
       <Text>{JSON.stringify(program)}</Text>
