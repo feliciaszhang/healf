cb6b9674ce302133491ad8a55e1e9eac5b81f73c
feliciaszhang <feliciaszhang@gmail.com>
2021-02-13 05:23:56 -0500

    add save button

--- pages/import.tsx
+++ pages/import.tsx
@@ -2,7 +2,7 @@ import React, { useState, useMemo } from "react";
 import { useRouter } from "next/router";
 import useAdmin from "../lib/useAdmin";
 import Layout from "../components/Layout";
-import { Text, Container } from "@chakra-ui/react";
+import { Text, Container, Button } from "@chakra-ui/react";
 import Table from "../components/Table";
 import { useDropzone } from "react-dropzone";
 import XLSX from "xlsx";
@@ -30,19 +30,19 @@ const Import = () => {
     color: "#bdbdbd",
     outline: "none",
     transition: "border .24s ease-in-out",
-  };
+  } as const;
 
   const activeStyle = {
     borderColor: "#2196f3",
-  };
+  } as const;
 
   const acceptStyle = {
     borderColor: "#00e676",
-  };
+  } as const;
 
   const rejectStyle = {
     borderColor: "#ff1744",
-  };
+  } as const;
 
   const {
     getRootProps,
@@ -88,7 +88,25 @@ const Import = () => {
   return (
     <Layout>
       {program.length ? (
-        <Table data={program} />
+        <>
+          <Container centerContent={true}>
+            <Table data={program} />
+          </Container>
+          <Button
+            zIndex={1}
+            position="sticky"
+            background="white"
+            bottom={0}
+            colorScheme="blackAlpha"
+            variant="outline"
+            onClick={async () => {
+              await mutateUser(fetch("/api/save", { method: "POST" }));
+              router.push("/");
+            }}
+          >
+            Save
+          </Button>
+        </>
       ) : (
         <Container {...getRootProps({ style })}>
           <input {...getInputProps()} />
